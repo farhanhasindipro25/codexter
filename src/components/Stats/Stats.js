2149fd7d4fe188a56ff6313588a5b68210215591
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2022-10-12 23:09:26 +0600

    Number of total questions per topic displayed in a line chart on UI, also made responsive

--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -1,16 +1,51 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
-import TotalQuestionsChart from "../TotalQuestionsChart/TotalQuestionsChart";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 
 const Stats = () => {
   const stats = useLoaderData();
   console.log(stats);
 
+  // stats.data.map((item) => console.log(item));
+
+  const data = [
+    {
+      name: stats.data[0].name,
+      total: stats.data[0].total,
+    },
+    {
+      name: stats.data[1].name,
+      total: stats.data[1].total,
+    },
+    {
+      name: stats.data[2].name,
+      total: stats.data[2].total,
+    },
+    {
+      name: stats.data[3].name,
+      total: stats.data[3].total,
+    },
+  ];
+
   return (
-    <div>
-      {stats.data.map((item) => (
-        <TotalQuestionsChart key={item.id} item={item}></TotalQuestionsChart>
-      ))}
+    <div className="container mx-auto pb-96 mt-36">
+      <ResponsiveContainer className="mx-auto" width="80%" height={400}>
+        <LineChart data={data}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Line type="monotone" dataKey="total" stroke="#82ca9d" />
+        </LineChart>
+      </ResponsiveContainer>
     </div>
   );
 };
